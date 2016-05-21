ad6e734fba585210b82cb0ec38266d6678f38d49
Matt Robinson <brimstone@the.narro.ws>
2016-05-21 09:29:01 -0400

    Adding jquery and a little bootstrap

--- client/main.go
+++ client/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/gopherjs/jquery"
+)
+
+const (
+	// INPUT holds our user's name
+	INPUT = "input#name"
+	// OUTPUT holds greeting
+	OUTPUT = "span#output"
+)
+
+var jQuery = jquery.NewJQuery
 
 func main() {
 	log.Println("Hello world")
+	jQuery(INPUT).On(jquery.KEYUP, func(e jquery.Event) {
+
+		name := jQuery(e.Target).Val()
+		name = jquery.Trim(name)
+
+		//show welcome message:
+		if len(name) > 0 {
+			jQuery(OUTPUT).SetText("Welcome to GopherJS, " + name + "!")
+		} else {
+			jQuery(OUTPUT).Empty()
+		}
+	})
+
 }
