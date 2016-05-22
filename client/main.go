9dbbde963176a3db4a78239d0e861bc6066c7aa8
Matt Robinson <brimstone@the.narro.ws>
2016-05-21 21:43:04 -0400

    Super cool

--- client/main.go
+++ client/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 
+	"github.com/gopherjs/websocket"
+
+	"github.com/gopherjs/gopherjs/js"
 	"github.com/gopherjs/jquery"
 )
 
@@ -15,8 +18,43 @@ const (
 
 var jQuery = jquery.NewJQuery
 
+func websocketAddress() string {
+	location := js.Global.Get("window").Get("location")
+	protocol := location.Get("protocol").String()
+	host := location.Get("host").String()
+
+	if protocol == "http:" {
+		protocol = "ws:"
+	} else {
+		protocol = "wss:"
+	}
+
+	return protocol + "//" + host + "/ws"
+}
+
+func websocketSetup() error {
+	c, err := websocket.Dial(websocketAddress())
+	if err != nil {
+		return err
+	}
+	log.Println("Websocket open")
+
+	jQuery(INPUT).On(jquery.KEYUP, func(e jquery.Event) {
+		name := jQuery(e.Target).Val()
+		name = jquery.Trim(name)
+		log.Println("Writing to the server")
+		_, err = c.Write([]byte("User entered:" + name))
+	})
+
+	return nil
+}
+
 func main() {
+
 	log.Println("Hello world")
+
+	go websocketSetup()
+
 	jQuery(INPUT).On(jquery.KEYUP, func(e jquery.Event) {
 
 		name := jQuery(e.Target).Val()
@@ -30,4 +68,19 @@ func main() {
 		}
 	})
 
+	/*
+	   c, err := websocket.Dial("ws://localhost/socket") // Blocks until connection is established
+	   if err != nil { panic(err) }
+
+	   buf := make([]byte, 1024)
+	   n, err = c.Read(buf) // Blocks until a WebSocket frame is received
+	   if err != nil { panic(err) }
+	   doSomethingWithData(buf[:n])
+
+	   _, err = c.Write([]byte("Hello!"))
+	   if err != nil { panic(err) }
+
+	   err = c.Close()
+	   if err != nil { panic(err) }
+	*/
 }
--- main.go
+++ main.go
@@ -7,12 +7,51 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/shurcooL/httpgzip"
+	"golang.org/x/net/websocket"
 )
 
+func readWebsocket(ws *websocket.Conn, outChan chan string, errChan chan error) {
+	var message string
+	var err error
+	for {
+		// read in a message
+		err = websocket.Message.Receive(ws, &message)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		// send it out of our channel
+		outChan <- message
+	}
+}
+func websocketHandler(ws *websocket.Conn) {
+	// when we exit the function, close the socket
+	defer ws.Close()
+	// start our reader in the background
+	clientmsg := make(chan string)
+	clienterr := make(chan error)
+	//websocket.Message.Send(ws, "Howdy ho!")
+	go readWebsocket(ws, clientmsg, clienterr)
+	for {
+		select {
+		case data := <-clientmsg:
+			log.Println("Received:", data)
+		case e := <-clienterr:
+			if e.Error() != "EOF" {
+				log.Println("Error from websocket: ", e.Error())
+			}
+			return
+
+		}
+	}
+}
+
 func main() {
+	http.Handle("/ws", websocket.Handler(websocketHandler))
 	http.Handle("/assets/", httpgzip.FileServer(assets, httpgzip.FileServerOptions{ServeError: httpgzip.Detailed}))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
